a01c3e231c6742e4c74c2aa07f6e0557a81d9d21
Gokulk7 <gokulsk.m@gmail.com>
2019-06-18 18:56:46 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,6 @@
 echo "Build and compile is running"
+echo " Build/Compile completed successfully...."
+sleep 2s
+echo "copy files..."
+cp -rf Compile_script.bat	README.md	lnCompile.bat Deploy
+tar -czvf binary.tar.gz Deploy
